575e95a1798c3d05d09ae801db571913eeb32599
Cody Evol <cody@evolcreative.com>
2015-10-26 00:53:40 -0400

    Create Flow + Sign out

---
+++ DropboxDemo/SettingScreen.swift
@@ -0,0 +1,42 @@
+//
+//  SettingScreen.swift
+//  DropboxDemo
+//
+//  Created by Cody Evol on 10/26/15.
+//  Copyright © 2015 Cody Evol. All rights reserved.
+//
+
+import UIKit
+
+class SettingScreen: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+          scrollView.contentSize = imageView.frame.size
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func SignOut(sender: AnyObject) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
